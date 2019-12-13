b0d20e98b74b6d387fcd842768b55a6a45dbc0ff
tejsubedi@gmail.com <tejsubedi@gmail.com>
2019-12-12 21:21:51 -0500

    Project completed.

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
 
                 Random randomNumberGenerator = new Random();
-                int number = randomNumberGenerator.nextInt(4);
+                int number = randomNumberGenerator.nextInt(5);
 
                 ballDisplay.setImageResource(ballArray[number]);
 
